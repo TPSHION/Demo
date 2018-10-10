1fe70feba34bcfc78f0fd27555972dcd52f8b5c1
TPSHION <1059457911@qq.com>
2018-10-10 16:13:58 +0800

    增加打印金字塔功能

--- PyramidDemo/src/com/pyramid/PyramidDemo.java
+++ PyramidDemo/src/com/pyramid/PyramidDemo.java
@@ -1,7 +1,26 @@
 package com.pyramid;
 
+import java.util.Scanner;
+
 public class PyramidDemo {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入要打印金字塔的层数：");
+        int num = scanner.nextInt();
+        printPyramid(num);
+        scanner.close();
+    }
+    
+    public static void printPyramid(int num) {
+        for(int i = 1; i < num+1; i++) {
+            for(int j = 0; j < num-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < 2*i-1; k++) {
+                System.out.print("▲");
+            }
+            System.out.print("\n");  
+        }
+        
     }
 }
